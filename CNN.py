968c80574e9330aa66f02424986f38dabbcba6cb
lizihan <lizihan@umich.edu>
2017-11-23 15:07:08 -0500

    init

---
+++ CNN.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import numpy as np
+import csv, datetime, time, json
+from zipfile import ZipFile
+from os.path import expanduser, exists
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+from keras.layers import Input, TimeDistributed, Dense, Lambda, concatenate, Dropout, BatchNormalization
+from keras.layers.embeddings import Embedding
+from keras.regularizers import l2
+from keras.callbacks import Callback, ModelCheckpoint
+from keras.utils.data_utils import get_file
+from keras import backend as K
+from sklearn.model_selection import train_test_split
+
+# Initialize global variables
